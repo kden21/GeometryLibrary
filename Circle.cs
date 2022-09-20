2db96d4b8982d0f52e7ad9114f96fe7696b78190
kden <ks230den@gmail.com>
2022-09-20 16:52:02 +0300

    Добавьте файлы проекта.

---
+++ Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryLibrary
+{
+    public class Circle : IShape
+    {
+        double radius;
+        public Circle(double radius)
+        {
+            this.radius = radius;
+            if(radius <= 0)
+            {
+                throw new ArgumentException("Значение радиуса должно быть положительным числом");
+            }
+        }
+        public double Area()
+        {
+            return Math.Pow(radius, 2) * Math.PI;
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryLibrary
+{
+    public class Triangle : IShape
+    {
+        double a;
+        double b;
+        double c;
+        public Triangle(double a, double b, double c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                throw new ArgumentException("Значение длины стороны должно быть положительным числом");
+            }
+            if (((a + b) < c) || (a + c) < b || (b + c) < a)
+            {
+                throw new ArgumentException("Одна сторона треугольника больше суммы двух других сторон. Такой треугольник не может существовать");
+            }
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public double Area()
+        {
+            double halfPerimeter = (a + b + c)/2;
+            return Math.Sqrt(halfPerimeter * (halfPerimeter-a) * (halfPerimeter-b) * (halfPerimeter-c));
+        }
+
+        public bool RightTriangle()
+        {
+            double area = Area();
+            if ((area == (a*b)/2) || (area == (a * c) / 2) || (area == (c * b) / 2))
+                return true;
+            return false;
+        }
+    }
+}
